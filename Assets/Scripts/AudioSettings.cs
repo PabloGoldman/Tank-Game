8ab7124746e87fbc6b870b77576280d8f49ab9a2
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-07-19 16:45:58 -0300

    Added AudioMixers.

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public AudioMixer SFXMixer;
+
+    private const string volumeKey = "Volume";
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat(volumeKey, Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        SFXMixer.SetFloat(volumeKey, Mathf.Log10(volume) * 20);
+    }
+}
