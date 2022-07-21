1738f81ff5e900990c7da06d76123a70db7cf827
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-07-21 15:02:54 -0300

    Added bullet FBX.

---
+++ Assets/Scripts/Bullet Scripts/BulletManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+
+    [SerializeField] Transform spawnPoint;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnBullet()
+    {
+
+    }
+}
